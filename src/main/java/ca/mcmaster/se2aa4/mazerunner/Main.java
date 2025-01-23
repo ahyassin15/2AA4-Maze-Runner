b2c57b62c60c74df5fa6e289acfc1a9eefbc4e23
Ahmed <ah.yassin05@gmail.com>
2025-01-22 21:27:36 -0500

    Added MacID to pom.xml and made edits to starter code

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,34 +3,68 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.nio.file.Path;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+
+        logger.info("** Starting Maze Runner");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
-                    }
-                }
-                System.out.print(System.lineSeparator());
-            }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            cmd = parser.parse(getParserOptions(), args);
+            String filePath = cmd.getOptionValue('i');
+            // Maze maze = new Maze(filePath);
+
+            // if (cmd.getOptionValue("p") != null) {
+            //     logger.info("Validating path");
+            //     Path path = new Path(cmd.getOptionValue("p"));
+            //     if (maze.validatePath(path)) {
+            //         System.out.println("correct path");
+            //     } else {
+            //         System.out.println("incorrect path");
+            //     }
+            // } else {
+            //     String method = cmd.getOptionValue("method", "righthand");
+            //     Path path = solveMaze(method, maze);
+            //     System.out.println(path.getFactorizedForm());
+            // }
+        } catch (Exception e) {
+            System.err.println("MazeSolver failed.  Reason: " + e.getMessage());
+            logger.error("MazeSolver failed.  Reason: " + e.getMessage());
+            logger.error("PATH NOT COMPUTED");
         }
+
+        logger.info("End of MazeRunner");
+
         System.out.println("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
         System.out.println("** End of MazeRunner");
+    }
+
+    /**
+     * Get options for CLI parser.
+     *
+     * @return CLI parser options
+     */
+    private static Options getParserOptions() {
+        Options options = new Options();
+
+        Option fileOption = new Option("i", true, "File that contains maze");
+        fileOption.setRequired(true);
+        options.addOption(fileOption);
+
+        options.addOption(new Option("p", true, "Path to be verified in maze"));
+        options.addOption(new Option("method", true, "Specify which path computation algorithm will be used"));
+
+        return options;
     }
 }
