59d5dd235a0734e2907c61e7b3d93d26e8173b65
Ahmed <ah.yassin05@gmail.com>
2025-01-24 23:15:08 -0500

    Completed ;2CMaze class to scan and manage a 2D maze from file input

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -29,7 +29,7 @@ public class Main {
                 String inputFile = cmd.getOptionValue("i");
                 logger.info("**** Reading the maze from file " + inputFile);
                 maze = new Maze(inputFile);
-                mazeRunner = new MazeRunner(maze.getMazeArray(), maze.getEntryPoint(), maze.getExitPoint());
+                mazeRunner = new MazeRunner(maze.getMazeGrid(), maze.getEntryPoint(), maze.getExitPoint());
                 
                 if (mazeRunner.MazeAlgorithm()) {
                     logger.info("Maze solved successfully.");
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -7,32 +7,39 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Maze {
+    
     private static final Logger logger = LogManager.getLogger();
-    String[][] mazeArray;
+    String[][] mazeGrid;
 
     public Maze(String inputFile) {
         scanMaze(inputFile);
     }
 
     public String[][] scanMaze(String inputFile) {
+        
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
             String line;
             int rowCount = 0;
+            
             while ((line = reader.readLine()) != null) {
                 rowCount++;
             }
+
             reader.close();
 
-            mazeArray = new String[rowCount][];
+            mazeGrid = new String[rowCount][];
             BufferedReader reader2 = new BufferedReader(new FileReader(inputFile));
             int row = 0;
+
             while ((line = reader2.readLine()) != null) {
-                mazeArray[row] = new String[line.length()];
+                
+                mazeGrid[row] = new String[line.length()];
+                
                 for (int index = 0; index < line.length(); index++) {
                     if (line.charAt(index) == '#') {
-                        mazeArray[row][index] = "#";
+                        mazeGrid[row][index] = "#";
                     } else if (line.charAt(index) == ' ') {
-                        mazeArray[row][index] = " ";
+                        mazeGrid[row][index] = " ";
                     }
                 }
                 row++;
@@ -42,29 +49,29 @@ public class Maze {
         } catch (IOException e) {
             logger.error("An error occurred while reading the maze file", e);
         }
-        return mazeArray;
+        return mazeGrid;
     }
 
-    public String[][] getMazeArray() {
-        return mazeArray;
+    public String[][] getMazeGrid() {
+        return mazeGrid;
     }
 
-    public void setMazeArray(String[][] mazeArray) {
-        this.mazeArray = mazeArray;
+    public void setMazeArray(String[][] mazeGrid) {
+        this.mazeGrid = mazeGrid;
     }
 
     public void printMaze() {
-        for (int i = 0; i < mazeArray.length; i++) {
-            for (int j = 0; j < mazeArray[i].length; j++) {
-                logger.info(mazeArray[i][j]);
+        for (int i = 0; i < mazeGrid.length; i++) {
+            for (int j = 0; j < mazeGrid[i].length; j++) {
+                logger.info(mazeGrid[i][j]);
             }
             logger.info(System.lineSeparator());
         }
     }
 
     public int[] getEntryPoint() {
-        for (int i = 0; i < mazeArray.length; i++) {
-            if (mazeArray[i][0].equals(" ")) {
+        for (int i = 0; i < mazeGrid.length; i++) {
+            if (mazeGrid[i][0].equals(" ")) {
                 return new int[]{i, 0};
             }
         }
@@ -72,9 +79,9 @@ public class Maze {
     }
 
     public int[] getExitPoint() {
-        for (int i = 0; i < mazeArray.length; i++) {
-            if (mazeArray[i][mazeArray[i].length - 1].equals(" ")) {
-                return new int[]{i, mazeArray[i].length - 1};
+        for (int i = 0; i < mazeGrid.length; i++) {
+            if (mazeGrid[i][mazeGrid[i].length - 1].equals(" ")) {
+                return new int[]{i, mazeGrid[i].length - 1};
             }
         }
         return null;
